f0474c5f1a95ab2acde0ff34ef4e3efd13e45062
NDOB <66097352+Linux-NDOB@users.noreply.github.com>
2022-05-23 19:21:03 -0500

    Database Folder
    All related to database design , MER , MR ans SQL scripts.

---
+++ Designs/database_script.sql
@@ -0,0 +1,10 @@
+CREATE TABLE VITALS (
+  vitals_id INTEGER UNSIGNED  NOT NULL   AUTO_INCREMENT,
+  oxigen INTEGER UNSIGNED  NOT NULL  ,
+  heart_rate INTEGER UNSIGNED  NOT NULL  ,
+  temperature FLOAT  NOT NULL    ,
+PRIMARY KEY(vitals_id));
+
+
+
+
